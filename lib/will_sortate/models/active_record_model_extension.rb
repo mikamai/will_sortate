d1e6750b047fbf6ee39dd3729cb4e91dbe4ecd50
Nicola Racco <nicola@nicolaracco.com>
2013-06-06 09:42:32 +0200

    match also dots and scores

--- lib/will_sortate/models/active_record_model_extension.rb
+++ lib/will_sortate/models/active_record_model_extension.rb
@@ -3,7 +3,7 @@ module WillSortate
     attr_reader :sortated_field, :sortated_order
 
     def sortate order_by, default_order_by
-      regexp = /(\w+)_(asc|desc)$/
+      regexp = /([\w\.-]+)_(asc|desc)$/
       md = order_by.try(:match, regexp) || default_order_by.match(regexp)
       raise "OrderBy argument has to be specified in the format :field_:direction" unless md
 
